2d5b7d50e487bcc1128f9010ddec3ac3a4fb597e
송광호 <skh990427@naver.com>
2026-01-11 00:29:36 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.11 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,33 +1,26 @@
 class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
+        int num = 1;
+        int x = 0, y = 0;
+        int dx[] = {0, 1, 0, -1};
+        int dy[] = {1, 0, -1, 0};
+        int direction = 0;
         
-        int max = n / 2;
-        if(n % 2 != 0) {
-            answer[max][max] = n*n;
-        }
-        
-        int cnt = 1;
-        for(int i = 0 ; i < max ; i++) {
-            int x = i;
-            int y = i;
+        while(num <= n * n) {
+            answer[x][y] = num++;
             
-            // right
-            for(; y < n-(i+1);y++) {
-                answer[x][y] = cnt++;
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+            
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n || answer[nx][ny] != 0) {
+                direction = (direction + 1) % 4;
+                nx = x + dx[direction];
+                ny = y + dy[direction];
             }
-            // down
-            for(; x < n-(i+1);x++) {
-                answer[x][y] = cnt++;
-            }
-            //left
-            for(; y > i; y--){
-                answer[x][y] = cnt++;
-            }
-            //up
-            for(; x > i; x--) {
-                answer[x][y] = cnt++;
-            }
+            
+            x = nx;
+            y = ny;
         }
         return answer;
     }
