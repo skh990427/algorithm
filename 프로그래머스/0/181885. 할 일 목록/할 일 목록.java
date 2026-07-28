1d07c9dab5baa5ebb63d6ea3fc23f689f44f6492
송광호 <skh990427@naver.com>
2026-07-28 20:54:54 +0900

    [level 0] Title: 할 일 목록, Time: 1.28 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> result = new ArrayList<>();
+        
+        for(int i = 0 ; i < finished.length ; i++) {
+            if(!finished[i])
+                result.add(todo_list[i]);
+        }
+        
+        return result.toArray(String[]::new);
+    }
+}
