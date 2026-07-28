2d21d14405c10fbda6a576148c7e90635729d5f8
송광호 <skh990427@naver.com>
2026-07-28 21:00:16 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.04 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int sumOdd = 0;
+        int sumEven = 0;
+        
+        for(int i = 0 ; i < num_list.length ; i++) {
+            if((i + 1) % 2 == 0) {
+                sumOdd += num_list[i];
+            } else {
+                sumEven += num_list[i];
+            }
+        }
+        
+        return Math.max(sumOdd, sumEven);
+    }
+}
