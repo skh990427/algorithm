5f9c9918b3dbf4a6ffee88e1095e4a72ce3ed9c9
송광호 <skh990427@naver.com>
2026-07-28 22:07:30 +0900

    [level 0] Title: 접미사 배열, Time: 0.53 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] result = new String[my_string.length()];
+        for(int i = 0 ; i < my_string.length() ; i++) {
+            result[i] = my_string.substring(i, my_string.length());
+        }
+        Arrays.sort(result);
+        
+        return result;
+    }
+}
