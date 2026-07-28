b60cb229bf7e4c82a9bb54caacf4d41d8afbefc6
송광호 <skh990427@naver.com>
2026-07-28 20:35:36 +0900

    [level 0] Title: ad 제거하기, Time: 0.54 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> result = new ArrayList<>();
+        for(String str : strArr) {
+            if(!str.contains("ad"))
+                result.add(str);
+        }
+        
+        return result.toArray(new String[result.size()]);
+    }
+}
