7e6006e208cfd70e08357fefb40eb9b9740432eb
송광호 <skh990427@naver.com>
2026-07-28 22:50:56 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int sum = 0;
+        int multi = 1;
+        for(int num : num_list) {
+            sum += num;
+            multi *= num;
+        }
+        
+        return multi < sum * sum ? 1 : 0;
+    }
+}
