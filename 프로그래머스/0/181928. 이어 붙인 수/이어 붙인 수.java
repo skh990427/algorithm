e7b57d9402221a87ba88f5729cae6c64bb1fc1e0
송광호 <skh990427@naver.com>
2026-07-28 22:57:55 +0900

    [level 0] Title: 이어 붙인 수, Time: 6.94 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+        for(int num : num_list) {
+            if(num % 2 == 1)
+                odd += num;
+            else
+                even += num;
+        }
+        return Integer.parseInt(odd) + Integer.parseInt(even);
+    }
+}
