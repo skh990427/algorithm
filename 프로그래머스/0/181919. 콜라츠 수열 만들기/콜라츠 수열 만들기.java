a7045bf52d3bd4b927363f49e87618e0184168cd
송광호 <skh990427@naver.com>
2026-07-28 22:15:23 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 2.87 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> result = new ArrayList<>();
+        result.add(n);
+        while (n != 1) {
+            if(n % 2 == 0)
+                n /= 2;
+            else
+                n = 3 * n + 1;
+            result.add(n);
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
