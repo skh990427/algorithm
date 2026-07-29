5bb84738cd634f07279b98a0d2a76c84066bda36
송광호 <skh990427@naver.com>
2026-07-29 19:30:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int num) {
+        int count = 0;
+        long n = num;
+        while(n != 1) {
+            if(n % 2 == 0)
+                n = n / 2;
+            else
+                n = (n * 3) + 1;
+            count++;
+            
+            if(count == 500) return -1;
+        }
+        return count;
+    }
+}
